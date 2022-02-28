b4f3570256fc3f7bd2068d5c4a73ce2525ff6b94
klaida <66129477+klaidaazizi@users.noreply.github.com>
2022-02-28 14:14:43 -0500

    a5 JavaScript sp22

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -1,6 +1,6 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import ExploreComponent from "./ExploreComponent.js";
-import WhoToFollowList from "../WhoToFollowList/who.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
 
 (function ($) {
     $('#wd-explore').append(`
